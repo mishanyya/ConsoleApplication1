12b0ea3d0c328f63745ba4bf98d6d949d5e2af05
mishanyya <mishanyakashin@gmail.com>
2019-08-14 06:58:19 +0300

    14,08 06,58
    должен показывать вариант ОС, но не показывает

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,17 +1,19 @@
 ﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
-
-/*#include <iostream>
+/*
+#include <iostream>
 
 int main()
 {
-	int x;
+	int x,y;
 	std::cout << "insert value of x:";
-	std::cin >> x;
-	x = 2 + 2 * x;
+	std::cin >> x>>y;
+	x = x * x;
     std::cout <<"x="<< x;
 	getchar();
 }*/
+
+
 
 
 #define _WIN32_DCOM
@@ -26,42 +28,45 @@ int main(int argc, char** argv)
 {
 	HRESULT hres;
 
-	// Initialize COM.
+	// Step 1: --------------------------------------------------
+	// Initialize COM. ------------------------------------------
+
 	hres = CoInitializeEx(0, COINIT_MULTITHREADED);
 	if (FAILED(hres))
 	{
-		cout << "Failed to initialize COM library. "
-			<< "Error code = 0x"
+		cout << "Failed to initialize COM library. Error code = 0x"
 			<< hex << hres << endl;
-		return 1;              // Program has failed.
+		return 1;                  // Program has failed.
 	}
 
-	// Initialize 
+	// Step 2: --------------------------------------------------
+	// Set general COM security levels --------------------------
+
 	hres = CoInitializeSecurity(
 		NULL,
-		-1,      // COM negotiates service                  
-		NULL,    // Authentication services
-		NULL,    // Reserved
-		RPC_C_AUTHN_LEVEL_DEFAULT,    // authentication
-		RPC_C_IMP_LEVEL_IMPERSONATE,  // Impersonation
-		NULL,             // Authentication info 
-		EOAC_NONE,        // Additional capabilities
-		NULL              // Reserved
+		-1,                          // COM authentication
+		NULL,                        // Authentication services
+		NULL,                        // Reserved
+		RPC_C_AUTHN_LEVEL_DEFAULT,   // Default authentication 
+		RPC_C_IMP_LEVEL_IMPERSONATE, // Default Impersonation  
+		NULL,                        // Authentication info
+		EOAC_NONE,                   // Additional capabilities 
+		NULL                         // Reserved
 	);
 
 
 	if (FAILED(hres))
 	{
-		cout << "Failed to initialize security. "
-			<< "Error code = 0x"
+		cout << "Failed to initialize security. Error code = 0x"
 			<< hex << hres << endl;
 		CoUninitialize();
-		return 1;          // Program has failed.
+		return 1;                    // Program has failed.
 	}
 
-	// Obtain the initial locator to Windows Management
-	// on a particular host computer.
-	IWbemLocator* pLoc = 0;
+	// Step 3: ---------------------------------------------------
+	// Obtain the initial locator to WMI -------------------------
+
+	IWbemLocator* pLoc = NULL;
 
 	hres = CoCreateInstance(
 		CLSID_WbemLocator,
@@ -71,29 +76,30 @@ int main(int argc, char** argv)
 
 	if (FAILED(hres))
 	{
-		cout << "Failed to create IWbemLocator object. "
-			<< "Error code = 0x"
+		cout << "Failed to create IWbemLocator object."
+			<< " Err code = 0x"
 			<< hex << hres << endl;
 		CoUninitialize();
-		return 1;       // Program has failed.
+		return 1;                 // Program has failed.
 	}
 
-	IWbemServices* pSvc = 0;
+	// Step 4: -----------------------------------------------------
+	// Connect to WMI through the IWbemLocator::ConnectServer method
 
-	// Connect to the root\cimv2 namespace with the
-	// current user and obtain pointer pSvc
+	IWbemServices* pSvc = NULL;
+
+	// Connect to the root\cimv2 namespace with
+	// the current user and obtain pointer pSvc
 	// to make IWbemServices calls.
-
 	hres = pLoc->ConnectServer(
-
-		_bstr_t(L"ROOT\\CIMV2"), // WMI namespace
-		NULL,                    // User name
-		NULL,                    // User password
-		0,                       // Locale
-		NULL,                    // Security flags                 
-		0,                       // Authority       
-		0,                       // Context object
-		&pSvc                    // IWbemServices proxy
+		_bstr_t(L"ROOT\\CIMV2"), // Object path of WMI namespace
+		NULL,                    // User name. NULL = current user
+		NULL,                    // User password. NULL = current
+		0,                       // Locale. NULL indicates current
+		NULL,                    // Security flags.
+		0,                       // Authority (for example, Kerberos)
+		0,                       // Context object 
+		&pSvc                    // pointer to IWbemServices proxy
 	);
 
 	if (FAILED(hres))
@@ -107,18 +113,19 @@ int main(int argc, char** argv)
 
 	cout << "Connected to ROOT\\CIMV2 WMI namespace" << endl;
 
-	// Set the IWbemServices proxy so that impersonation
-	// of the user (client) occurs.
-	hres = CoSetProxyBlanket(
 
-		pSvc,                         // the proxy to set
-		RPC_C_AUTHN_WINNT,            // authentication service
-		RPC_C_AUTHZ_NONE,             // authorization service
-		NULL,                         // Server principal name
-		RPC_C_AUTHN_LEVEL_CALL,       // authentication level
-		RPC_C_IMP_LEVEL_IMPERSONATE,  // impersonation level
-		NULL,                         // client identity 
-		EOAC_NONE                     // proxy capabilities     
+	// Step 5: --------------------------------------------------
+	// Set security levels on the proxy -------------------------
+
+	hres = CoSetProxyBlanket(
+		pSvc,                        // Indicates the proxy to set
+		RPC_C_AUTHN_WINNT,           // RPC_C_AUTHN_xxx
+		RPC_C_AUTHZ_NONE,            // RPC_C_AUTHZ_xxx
+		NULL,                        // Server principal name 
+		RPC_C_AUTHN_LEVEL_CALL,      // RPC_C_AUTHN_LEVEL_xxx 
+		RPC_C_IMP_LEVEL_IMPERSONATE, // RPC_C_IMP_LEVEL_xxx
+		NULL,                        // client identity
+		EOAC_NONE                    // proxy capabilities 
 	);
 
 	if (FAILED(hres))
@@ -131,55 +138,53 @@ int main(int argc, char** argv)
 		return 1;               // Program has failed.
 	}
 
+	// Step 6: --------------------------------------------------
+	// Use the IWbemServices pointer to make requests of WMI ----
 
-	// Use the IWbemServices pointer to make requests of WMI. 
-	// Make requests here:
-
-	// For example, query for all the running processes
+	// For example, get the name of the operating system
 	IEnumWbemClassObject* pEnumerator = NULL;
 	hres = pSvc->ExecQuery(
 		bstr_t("WQL"),
-		bstr_t("SELECT * FROM Win32_Process"),
+		bstr_t("SELECT * FROM Win32_OperatingSystem"),
 		WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY,
 		NULL,
 		&pEnumerator);
 
 	if (FAILED(hres))
 	{
-		cout << "Query for processes failed. "
-			<< "Error code = 0x"
+		cout << "Query for operating system name failed."
+			<< " Error code = 0x"
 			<< hex << hres << endl;
 		pSvc->Release();
 		pLoc->Release();
 		CoUninitialize();
 		return 1;               // Program has failed.
 	}
-	else
+
+	// Step 7: -------------------------------------------------
+	// Get the data from the query in step 6 -------------------
+
+	IWbemClassObject* pclsObj = NULL;
+	ULONG uReturn = 0;
+
+	while (pEnumerator)
 	{
-		IWbemClassObject* pclsObj;
-		ULONG uReturn = 0;
+		HRESULT hr = pEnumerator->Next(WBEM_INFINITE, 1,
+			&pclsObj, &uReturn);
 
-		while (pEnumerator)
+		if (0 == uReturn)
 		{
-			hres = pEnumerator->Next(WBEM_INFINITE, 1,
-				&pclsObj, &uReturn);
-
-			if (0 == uReturn)
-			{
-				break;
-			}
-
-			VARIANT vtProp;
-
-			// Get the value of the Name property
-			hres = pclsObj->Get(L"Name", 0, &vtProp, 0, 0);
-			wcout << "Process Name : " << vtProp.bstrVal << endl;
-			VariantClear(&vtProp);
-
-			pclsObj->Release();
-			pclsObj = NULL;
+			break;
 		}
 
+		VARIANT vtProp;
+
+		// Get the value of the Name property
+		hr = pclsObj->Get(L"Name", 0, &vtProp, 0, 0);
+		wcout << " OS Name : " << vtProp.bstrVal << endl;
+		VariantClear(&vtProp);
+
+		pclsObj->Release();
 	}
 
 	// Cleanup
@@ -188,16 +193,13 @@ int main(int argc, char** argv)
 	pSvc->Release();
 	pLoc->Release();
 	pEnumerator->Release();
-
 	CoUninitialize();
 
 	return 0;   // Program successfully completed.
+
 }
 
-
-//Process Name - показывает список процессов
-
-
+//должен показывать вариант ОС, но не показывает
 
 
 
